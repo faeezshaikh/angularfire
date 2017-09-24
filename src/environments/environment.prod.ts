192ae1379ae3a8786a3b44cffcbe2820730c7ac2
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-09-23 23:02:42 -0500

    deployed to fb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,18 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyBJsTnYKiu2mdLnwkSidQjw3THCty5sEm4',
-    authDomain: 'buildingappswithangularf-ebcb5.firebaseapp.com',
-    databaseURL: 'https://buildingappswithangularf-ebcb5.firebaseio.com',
-    projectId: 'buildingappswithangularf-ebcb5',
-    storageBucket: 'buildingappswithangularf-ebcb5.appspot.com',
-    messagingSenderId: '957445559070'
+    // apiKey: 'AIzaSyBJsTnYKiu2mdLnwkSidQjw3THCty5sEm4',
+    // authDomain: 'buildingappswithangularf-ebcb5.firebaseapp.com',
+    // databaseURL: 'https://buildingappswithangularf-ebcb5.firebaseio.com',
+    // projectId: 'buildingappswithangularf-ebcb5',
+    // storageBucket: 'buildingappswithangularf-ebcb5.appspot.com',
+    // messagingSenderId: '957445559070'
+
+    apiKey: "AIzaSyD97RFFjDIvSMFADiVmLv_nMpViatvh2jQ",
+    authDomain: "ameren-arb.firebaseapp.com",
+    databaseURL: "https://ameren-arb.firebaseio.com",
+    projectId: "ameren-arb",
+    storageBucket: "ameren-arb.appspot.com",
+    messagingSenderId: "292827807008"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,19 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBJsTnYKiu2mdLnwkSidQjw3THCty5sEm4',
-    authDomain: 'buildingappswithangularf-ebcb5.firebaseapp.com',
-    databaseURL: 'https://buildingappswithangularf-ebcb5.firebaseio.com',
-    projectId: 'buildingappswithangularf-ebcb5',
-    storageBucket: 'buildingappswithangularf-ebcb5.appspot.com',
-    messagingSenderId: '957445559070'
+    // apiKey: 'AIzaSyBJsTnYKiu2mdLnwkSidQjw3THCty5sEm4',
+    // authDomain: 'buildingappswithangularf-ebcb5.firebaseapp.com',
+    // databaseURL: 'https://buildingappswithangularf-ebcb5.firebaseio.com',
+    // projectId: 'buildingappswithangularf-ebcb5',
+    // storageBucket: 'buildingappswithangularf-ebcb5.appspot.com',
+    // messagingSenderId: '957445559070'
+
+    apiKey: "AIzaSyD97RFFjDIvSMFADiVmLv_nMpViatvh2jQ",
+    authDomain: "ameren-arb.firebaseapp.com",
+    databaseURL: "https://ameren-arb.firebaseio.com",
+    projectId: "ameren-arb",
+    storageBucket: "ameren-arb.appspot.com",
+    messagingSenderId: "292827807008"
   },
   fakeFirebaseConfig: {
     apiKey: 'xxx',
